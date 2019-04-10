b8a7fe0fe7c4ad11e37febbe9c10d6483bcf2268
sajan <sajanloveu123@gmail.com>
2019-04-10 21:50:25 +0530

    internet error handling

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import './Loading.css';
 import Main from "./Main";
 
 export default  class App extends Component {
+  state = {
+
+  }
+
   render() {
+    // var className;
+    // setTimeout(() => {this.setState({className: "internet-error"}); 
+    // setTimeout(() => this.setState({className: "remove-error"}), 2000 )}, 1000)
+
+    setTimeout(() => this.setState({className: "internet-error"}), 2000)
+    // setTimeout(() => (() => className = "internet-error")(), 2000)
     return (
-      <div className="main">
-        <Main />
-      </div>
+        navigator.onLine ? <Main /> :
+        <p className={`error ${this.state.className}`}>No internet connection</p>
     )
   }
 }
--- src/Cart.js
+++ src/Cart.js
@@ -17,7 +17,7 @@ class Cart extends React.Component {
 		return (
 			<React.Fragment>
 				<div>
-				<button className={`cart-btn ${this.state.className ? "click" : ""}`} key={uuid()} onClick={() => {this.props.toggleCart(Product.isOpen); this.setState({className: !this.state.className})}}>
+					<button className={`cart-btn ${Product.isOpen ? "drag_left" : "drag_right"}`} key={uuid()} onClick={() => { this.props.toggleCart(Product.isOpen); this.setState({ className: !this.state.className }) }}>
 						{
 							Product.isOpen ?
 								<span className="close-btn">X</span> :
@@ -27,53 +27,54 @@ class Cart extends React.Component {
 								</div>
 						}
 					</button>
-				<div className={`cart ${Product.isOpen ? "open" : "close"}`}>
-				
-					<div className="cart-header">
-						<div>
-							<i className="fas fa-cart-plus"></i>
-							<span className="product-quantity">{cart.length}</span>
+					<div className={`cart ${Product.isOpen ? "open" : "close"}`}>
+
+						<div className="cart-header">
+							<div>
+								<i className="fas fa-cart-plus"></i>
+								<span className="product-quantity">{cart.length}</span>
+							</div>
+							<h2 className="cart-text">Add something in cart</h2>
 						</div>
-						<h2 className="cart-text">Add something in cart</h2>
-					</div>
-					<div className="item-container">
-						<ul className="ul">
-							{(cart.length ? cart : []).map(item =>
-								(<li key={uuid()} className="shop-items">
-									<div className="item-size-details">
-										<img src={`https://raw.githubusercontent.com/jeffersonRibeiro/react-shopping-cart/master/src/static/products/${item.product.sku}_2.jpg`} alt="product_img" />
-										<div className="item-quantity">
-											<p>{item.product.title}</p>
-											<p><span>{item.product.availableSizes[0]}</span>{` | ${item.product.style}`}</p>
-											<p>Quantity : <span>{item.quant}</span></p>
+						<div className="item-container">
+							<ul className="ul">
+								{(cart.length ? cart : []).map(item =>
+									(<li key={uuid()} className="shop-items">
+										<div className="item-size-details">
+											<img src={`https://raw.githubusercontent.com/jeffersonRibeiro/react-shopping-cart/master/src/static/products/${item.product.sku}_2.jpg`} alt="product_img" />
+											<div className="item-quantity">
+												<p>{item.product.title}</p>
+												<p><span>{item.product.availableSizes[0]}</span>{` | ${item.product.style}`}</p>
+												<p>Quantity : <span>{item.quant}</span></p>
+											</div>
 										</div>
-									</div>
-									<div className="item-price">
-										<button className="remove-items" onClick={(e) => this.props.handleRemove(item.product.id)}>X</button>
-										<div className="per_item_price">
-											<span className="currency">{item.product.currencyFormat}</span>
-											<span>{(item.product.price * item.quant).toString().split(".")[0]} </span>
-											<span>
-												{(item.product.price * item.quant).toString().split(".")[1] ? (` .${(item.product.price * item.quant).toString().split(".")[1]}`) : ".00"}
-											</span>
+										<div className="item-price">
+											<button className="remove-items" onClick={(e) => this.props.handleRemove(item.product.id)}>X</button>
+											<div className="per_item_price">
+												<span className="currency">{item.product.currencyFormat}</span>
+												<span>{(item.product.price * item.quant).toString().split(".")[0]} </span>
+												<span>
+													{(item.product.price * item.quant).toString().split(".")[1] ? (` .${(item.product.price * item.quant).toString().split(".")[1]}`) : ".00"}
+												</span>
+											</div>
 										</div>
-									</div>
-								</li>)
-							)}
-						</ul>
-					</div>
-					<div className="subtotal-sec">
-						<div className="total-price">
-							<p>SUBTOTAL</p>
-							<p className="total_price" >
-								<span className="currency">$</span>
-								{total.toFixed(2)}
-							</p>
+									</li>)
+								)}
+							</ul>
 						</div>
-						<button className={`checkout-btn ${this.state.className}`} onClick={() => {
-							alert(total ? `$ ${total.toFixed(2)}` : "Add some items in cart")}}>CHECKOUT</button>
+						<div className="subtotal-sec">
+							<div className="total-price">
+								<p>SUBTOTAL</p>
+								<p className="total_price" >
+									<span className="currency">$</span>
+									{total.toFixed(2)}
+								</p>
+							</div>
+							<button className={`checkout-btn ${this.state.className}`} onClick={() => {
+								alert(total ? `$ ${total.toFixed(2)}` : "Add some items in cart")
+							}}>CHECKOUT</button>
+						</div>
 					</div>
-				</div>
 				</div>
 			</React.Fragment>
 		)
